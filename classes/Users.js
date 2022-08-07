772ee9767fd2c45d3b666302b131d979758cc45c
Carlos Gonzalez <siscond@hotmail.com>
2022-08-07 16:16:57 -0400

    Feature: condo and user name

--- classes/Users.js
+++ classes/Users.js
@@ -10,7 +10,9 @@ class Users{
             if( err ) return callback({ status: 500, message: err['sqlMessage'] });
         });
 
-        var sql = "select `id`, `email`, `name` from `users` where(`email`="+email+" and `pwd`="+pwd+" and status <> 0);";
+        var sql = "select c.`id` condo_id, c.`condo_name`, c.`logo`, u.`name`, u.`email`, u.`id` user_id from `condos` c inner join `condo_user` cu on c.`id` = cu.`id_condo` ";
+        sql += "inner join `users` u on cu.`id_user` = u.`id` and cu.id_user=(select `id` from `users` where(`email`="+email+" and `pwd`="+pwd+" and status <> 0));";
+        //var sql = "select `id`, `email`, `name` from `users` where(`email`="+email+" and `pwd`="+pwd+" and status <> 0);";
         con.query( sql, function( err, result ){
             con.end();
             if( err ){
@@ -18,9 +20,16 @@ class Users{
             }
 
             var user = null;
+            var uId = null;
             if( result ){
                 result.forEach(function(row) {
-                    user = '{"id":' + row.id+',"email":"'+row.email+'","name":"'+row.name+'"}';
+                    uId = row.id;
+                    user = '{"condo_id":' + row.condo_id+
+                        ',"condo_name":"'+row.condo_name+
+                        '","user_id":'+row.user_id+
+                        ',"email":"'+row.email+
+                        '","user_name":"'+row.name+
+                    '"}';
                 });
                 return callback ({ status:200, message:user });
             }
