1b5b6514c928ec159a2f8dd138326d993ee71ebd
Carlos Gonzalez <siscond@hotmail.com>
2023-01-18 05:35:39 -0500

    Fix: return next

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.post('/token', (req, res) => {
 		user.checkUserAccount( email, pwd, function( result ){
 				if( result.status != 200 ){
 					res.status( result.status ).send( result.message );
-					return;
+					return next();
 				}
 				next();
 			}, 
